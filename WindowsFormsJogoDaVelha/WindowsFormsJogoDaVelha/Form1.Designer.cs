ca9f7485da0e2ba0655961723712fd82b06ac578
Thiago <32786177+ThiagoPeresBezerra@users.noreply.github.com>
2018-11-28 21:27:03 -0200

    Add files via upload

---
+++ WindowsFormsJogoDaVelha/WindowsFormsJogoDaVelha/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WindowsFormsJogoDaVelha
+{
+    partial class Form1_Jogo_da_Velha
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1_Jogo_da_Velha));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_Reset = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button6
+            // 
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button6.Name = "button6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button9
+            // 
+            resources.ApplyResources(this.button9, "button9");
+            this.button9.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button9.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button9.Name = "button9";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            resources.ApplyResources(this.button8, "button8");
+            this.button8.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button8.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button8.Name = "button8";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button7.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button7.Name = "button7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Name = "label1";
+            // 
+            // button_Reset
+            // 
+            resources.ApplyResources(this.button_Reset, "button_Reset");
+            this.button_Reset.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button_Reset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_Reset.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.button_Reset.Name = "button_Reset";
+            this.button_Reset.UseVisualStyleBackColor = false;
+            this.button_Reset.Click += new System.EventHandler(this.button_Reset_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Name = "label2";
+            // 
+            // Form1_Jogo_da_Velha
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsJogoDaVelha.Properties.Resources.nvidia_grey_shape_774x435;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button_Reset);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.MaximizeBox = false;
+            this.Name = "Form1_Jogo_da_Velha";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_Reset;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsJogoDaVelha/WindowsFormsJogoDaVelha/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsJogoDaVelha
+{
+    public partial class Form1_Jogo_da_Velha : Form
+    {
+        public string jogador_atual = "X";
+
+        public Form1_Jogo_da_Velha()
+        {
+            InitializeComponent();
+        }
+
+        //Botão Reset.
+        private void button_Reset_Click(object sender, EventArgs e)
+        {
+            resetar();
+        }
+
+        //Form principal com label recebendo a variável jogador atual.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.ForeColor = System.Drawing.Color.Red;
+            label1.Text = "Jogador Atual: " + jogador_atual;            
+        }
+
+        //Método para inverter o jogador 'X' e 'O'.
+        public string inverter_jogador()
+        {
+            if (jogador_atual == "X")
+            {
+                label1.ForeColor = System.Drawing.Color.Blue;
+                return "O";
+            }
+            else
+            {
+                label1.ForeColor = System.Drawing.Color.Red;
+                return "X";
+            }           
+        }
+
+        //Método para zerar o jogo com o botão Reset e retornar o jogador atual 'X'.
+        public void resetar()
+        {
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button6.Text = "";
+            button5.Text = "";
+            button4.Text = "";
+            button9.Text = "";
+            button8.Text = "";
+            button7.Text = "";
+            jogador_atual = "X";
+            label1.ForeColor = System.Drawing.Color.Red;
+            label1.Text = "Jogador Atual: " + jogador_atual;
+        }
+
+        //Caixa de mensagem de jogador vencedor com método resetar.
+        public void vitoria()
+        {        
+            MessageBox.Show("O Jogador "+jogador_atual+" VENCEU!");
+            resetar();
+        }
+
+        //Função verificar vencedor.
+        public bool verificacao_vencedor(string jogador_atual_text)
+        {        
+            //Função Condicional Vertical.
+            if (button1.Text == jogador_atual_text && button6.Text == jogador_atual_text && button9.Text == jogador_atual_text)
+            {
+                vitoria();
+                return true;
+            }
+            if (button2.Text == jogador_atual_text && button5.Text == jogador_atual_text && button8.Text == jogador_atual_text)
+            {
+                vitoria();
+                return true;
+            }
+            if (button3.Text == jogador_atual_text && button4.Text == jogador_atual_text && button7.Text == jogador_atual_text)
+            {
+                vitoria();
+                return true;
+            }
+            //Função Condicional Horizontal.
+            if (button1.Text == jogador_atual_text && button2.Text == jogador_atual_text && button3.Text == jogador_atual_text)
+            {
+                vitoria();
+                return true;
+            }
+            if (button6.Text == jogador_atual_text && button5.Text == jogador_atual_text && button4.Text == jogador_atual_text)
+            {
+                vitoria();
+                return true;
+            }
+            if (button9.Text == jogador_atual_text && button8.Text == jogador_atual_text && button7.Text == jogador_atual_text)
+            {
+                vitoria();
+                return true;
+            }
+            //Função Condicional Diogonal
+            if (button1.Text == jogador_atual_text && button5.Text == jogador_atual_text && button7.Text == jogador_atual_text)
+            {
+                vitoria();
+                return true;
+            }
+            if (button3.Text == jogador_atual_text && button5.Text == jogador_atual_text && button9.Text == jogador_atual_text)
+            {
+                vitoria();
+                return true;
+            }
+            //Caixa de mensagem quando jogo der 'Velha'.
+            if (button1.Text.Length != 0 && button2.Text.Length != 0 && button3.Text.Length != 0 && button6.Text.Length != 0 && button5.Text.Length != 0 && button4.Text.Length != 0 && button9.Text.Length != 0 && button8.Text.Length != 0 && button7.Text.Length != 0)            
+            {
+                MessageBox.Show("Deu VELHA!");
+                resetar();
+                return true;
+            }
+            //Retornando jogador atual para 'X'.
+            jogador_atual = inverter_jogador();
+            label1.Text = "Jogador Atual: " + jogador_atual;
+            return false;
+        }
+        
+
+        //Verificando jogadas do jogador atual por botão.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "")
+            {
+                button1.Text = jogador_atual;
+                verificacao_vencedor(jogador_atual);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "")
+            {
+                button2.Text = jogador_atual;
+                verificacao_vencedor(jogador_atual);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Text == "")
+            {
+                button3.Text = jogador_atual;
+                verificacao_vencedor(jogador_atual);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button6.Text == "")
+            {
+                button6.Text = jogador_atual;
+                verificacao_vencedor(jogador_atual);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.Text == "")
+            {
+                button5.Text = jogador_atual;
+                verificacao_vencedor(jogador_atual);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (button4.Text == "")
+            {
+                button4.Text = jogador_atual;
+                verificacao_vencedor(jogador_atual);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (button9.Text == "")
+            {
+                button9.Text = jogador_atual;
+                verificacao_vencedor(jogador_atual);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (button8.Text == "")
+            {
+                button8.Text = jogador_atual;
+                verificacao_vencedor(jogador_atual);
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (button7.Text == "")
+            {
+                button7.Text = jogador_atual;
+                verificacao_vencedor(jogador_atual);
+            }
+        }
+    }
+}
